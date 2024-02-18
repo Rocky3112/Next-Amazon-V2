70a69450f2c922ddd375c58ab205c7707f40fa6d
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-02-18 16:24:49 +0600

    All setup done and starting

---
+++ app/(front)/layout.tsx
@@ -0,0 +1,8 @@
+
+export default function FrontLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <main className=" flex-grow container mx-auto px-5">{children}</main>
+}
---
+++ app/(front)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <h2 className="text-2xl py-2">Latest Products</h2>
+    </>
+  );
+}
