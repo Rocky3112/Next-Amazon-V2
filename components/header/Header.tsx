96f140f05fc32e5311be69ca59c27d395c2faae1
Rocky3112 <alomgirhossainrocky@gmail.com>
2024-02-18 16:48:42 +0600

    navbar created and data type declared

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,8 +1,25 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+        <nav>
+            <div className=' navbar justify-between bg-base-300 px-8'>
+                <Link href="/" className='btn btn-ghost bg-base-300 uppercase text-xl'>
+                Next Amazon V2
+                </Link>
+                <ul>
+                    <li>
+                        <Link href="/cart" className='btn btn-ghost '>Cart</Link>
+                    </li>
+                    <li>
+                        <Link href="/login" className='btn btn-ghost '>Login</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
   )
 }
 
---
+++ lib/modals/ProductModal.tsx
@@ -0,0 +1,16 @@
+export type Product = {
+    _id: string
+    name: string
+    image: string
+    slug: string
+    banner: string
+    brand: string
+    description: string
+    category: string
+    price: number
+    rating: number
+    numReviews: number
+    countInStock: number
+    color?: []
+    size?: []
+}
